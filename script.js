1349603490f60bac86c224c4f3a3f7f32230fcb5
Ar1f007 <muhammadarif9416@yahoo.com>
2021-06-21 22:29:08 +0600

    email validation and readme update

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const btn = document.querySelector(".form__btn");
 const inputGroup = document.querySelector(".input-group");
 const formInput = document.querySelector(".form__input");
+const errorMsg = document.querySelector(".form__error");
 
 const validateEmail = e => {
   e.preventDefault();
 
   const email = formInput.value;
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.length > 0) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if (re.test(email)) {
-    inputGroup.classList.remove("change");
-    formInput.value = "";
-  } else {
+    if (re.test(email)) {
+      inputGroup.classList.remove("change");
+      formInput.value = "";
+    } else {
+      inputGroup.classList.add("change");
+      errorMsg.textContent = "Please provide a valid email address";
+    }
+  }
+
+  if (email.length === 0) {
     inputGroup.classList.add("change");
+    errorMsg.textContent = "Whoops! It looks like you forgot to add your email";
   }
 };
 
