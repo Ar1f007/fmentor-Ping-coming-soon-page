738e255dec0523e6dbbfc5a3be53c68a64305036
Ar1f007 <muhammadarif9416@yahoo.com>
2021-08-12 03:13:54 +0600

    success message on subscribing

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const inputGroup = document.querySelector(".input-group");
 const formInput = document.querySelector(".form__input");
 const errorMsg = document.querySelector(".form__error");
 
-const validateEmail = e => {
+const removeMsg = () => {
+  setTimeout(() => {
+    inputGroup.classList.remove("change");
+  }, 2000);
+};
+
+const validateEmail = (e) => {
   e.preventDefault();
 
   const email = formInput.value;
@@ -12,7 +18,13 @@ const validateEmail = e => {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (re.test(email)) {
-      inputGroup.classList.remove("change");
+      inputGroup.classList.add("change");
+      errorMsg.style.color = "green";
+      formInput.style.borderColor = "green";
+      errorMsg.textContent = "Thanks for subscribing.";
+
+      removeMsg();
+
       formInput.value = "";
     } else {
       inputGroup.classList.add("change");
@@ -22,7 +34,9 @@ const validateEmail = e => {
 
   if (email.length === 0) {
     inputGroup.classList.add("change");
+    formInput.style.borderColor = "red";
     errorMsg.textContent = "Whoops! It looks like you forgot to add your email";
+    errorMsg.style.color = "var(--secondary-color-light-red)";
   }
 };
 
